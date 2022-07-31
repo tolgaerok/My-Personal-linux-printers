c2630548af5a0a44eaa663d0a9b7758492b7082c
Tolga Erok <110285959+tolgaerok@users.noreply.github.com>
2022-07-31 15:21:17 +0800

    Update install.sh

--- PRINTER FILES/INSTALL HL-2132/install.sh
+++ PRINTER FILES/INSTALL HL-2132/install.sh
@@ -1,31 +1,9 @@
 #! /bin/sh
-#  Copyright (C) 2015  SEIKO EPSON CORPORATION
-#
-#  License: GPL-3.0+
-#  Author : EPSON AVASYS CORPORATION
-#
-#  This program is free software: you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation, either version 3 of the License or, at
-#  your option, any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
-#
-#  You ought to have received a copy of the GNU General Public License
-#  along with this package.  If not, see <http://www.gnu.org/licenses/>.
+#  Copyright (C) 
+#  Install Brother HL-2132 Laser Drivers
+#  Tolga
 
-#   Convenience functions to deal with *.deb packages
 
-# Given a list of possibly versioned dependency alternatives, echo the
-# name of the preferred package to install if the dependency isn't met
-# already.  If the dependency is met, the empty string will be output.
-# This only works for dependencies that aim to add packages: Depends:,
-# Recommends: and Suggests:.
-# BUGS: The algorithm does not consider Provides:.
-#
 deb_to_install () {
 
     candidate=
