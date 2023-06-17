cadbb9f2420a28a2d68a5430146b0d91d56cf744
Tolga Erok <110285959+tolgaerok@users.noreply.github.com>
2023-06-18 04:53:03 +0800

    Update configuration.nix
    massive upgrade of events

--- LINUX RELATED/NIXOS 23-05 RELATED/System/BIOS/nixos/configuration.nix
+++ LINUX RELATED/NIXOS 23-05 RELATED/System/BIOS/nixos/configuration.nix
@@ -1,30 +1,30 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
-{
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-    ];
+let name = "tolga";
 
-  # Bootloader.
+in {
+  imports = [ ./hardware-configuration.nix ];
+
+  # Bootloader and System Settings
   boot.loader.grub.enable = true;
   boot.loader.grub.device = "/dev/sda";
   boot.loader.grub.useOSProber = true;
 
-  # Networking.
+  # Activate the automatic trimming process for SSDs on the NixOS system
+  services.fstrim.enable = true;
+
+  # Networking
   networking = {
     enableIPv6 = false;
     firewall.enable = false;
+    firewall.allowPing = true;
     hostName = "NixOs";
     networkmanager.enable = true;
   };
 
-  # Set your time zone.
+  # Time Zone and Locale
   time.timeZone = "Australia/Perth";
-
-  # Select internationalisation properties.
   i18n.defaultLocale = "en_AU.UTF-8";
-
   i18n.extraLocaleSettings = {
     LC_ADDRESS = "en_AU.UTF-8";
     LC_IDENTIFICATION = "en_AU.UTF-8";
@@ -37,23 +37,14 @@
     LC_TIME = "en_AU.UTF-8";
   };
 
-  # Enable the X11 windowing system.
+  # X11 and KDE Plasma
   services.xserver.enable = true;
-
-  # Enable the KDE Plasma Desktop Environment.
   services.xserver.displayManager.sddm.enable = true;
   services.xserver.desktopManager.plasma5.enable = true;
+  services.xserver.layout = "au";
+  services.xserver.xkbVariant = "";
 
-  # Configure keymap in X11
-  services.xserver = {
-    layout = "au";
-    xkbVariant = "";
-  };
-
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
-
-  # Enable sound with pipewire.
+  # Audio
   sound.enable = true;
   hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
@@ -63,58 +54,52 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-
   };
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.xxxxx = {  # Add your own user-name in place of xxxxx
+  # User Configuration
+  users.users.tolga = {
     isNormalUser = true;
-    description = "xxxxxxx"; # Add your full-name in place of the xxxxxxx
-    extraGroups = [ "networkmanager" "wheel" ];
-
-    packages = with pkgs; [
-      firefox
-      kate
-    ];
+    description = "tolga erok";
+    extraGroups = [ "networkmanager" "wheel" "input" "disk" "video" "audio" ];
+    packages = with pkgs; [ firefox kate digikam ];
   };
 
-  # Allow unfree packages
+  # Allow Unfree Packages
   nixpkgs.config.allowUnfree = true;
 
-  # Disable warnings, auto store optimization and enable experimental features
+  # Nix-specific settings and garbage collection options
   nix = {
     settings = {
-      warn-dirty = false;
+      allowed-users = [ "@wheel" ];
       auto-optimise-store = true;
-      experimental-features = ["nix-command" "flakes" "repl-flake"];
+      sandbox = true;
+      trusted-users = [ "root" "${name}" ];
+      warn-dirty = false;
+      experimental-features = [ "nix-command" "flakes" "repl-flake" ];
     };
 
-  # Enable automatic garbage collection, frequency of garbage collection and deleting older generations older-than 3d
     gc = {
       automatic = true;
       dates = "weekly";
-      options = "--delete-older-than 3d";
+      options = "--max-freed 1G --delete-older-than 7d";
     };
   };
 
-  # Add 'openssl-1.1.1u' to permitted insecure packages.
-  nixpkgs.config.permittedInsecurePackages = [
-    "openssl-1.1.1u"
-  ];
+  # Allow insecure or old pkgs
+  nixpkgs.config.permittedInsecurePackages =
+    [ "openssl-1.1.1u" "python-2.7.18.6" ];
 
-  # List packages installed in system profile. To search, run: nix search wget
+  # Nix package collection (pkgs) that you want to include in the system environment.
   environment.systemPackages = with pkgs; [
-    # Failed
-    # bluezUtilities
     asciiquarium
+    atool
     bat
     blueberry
     blueman
-    bluez
     bluez-tools
-    bluez5-experimental
     bottom
     btop
+    bzip2
     cachix
     caprine-bin
     cifs-utils
@@ -123,10 +108,12 @@
     coursier
     cowsay
     delta
+    dialog
     direnv
     doppler
     duf
     fd
+    ffmpeg
     figlet
     fx
     fzf
@@ -140,53 +127,81 @@
     graalvm17-ce
     graphviz
     gum
+    gzip
     heroku
     htop
     imagemagick
     ipfetch
+    isoimagewriter
     kate
+    kcalc
     keychain
     kitty
     kitty-themes
+    kphotoalbum
+    krename
     krita
     less
+    libarchive
     libbtbb
     lolcat
+    lz4
+    lzip
+    lzo
+    lzop
+    mariadb
     mosh
     mpv
     ncdu
     neofetch
     neovim
     nix-direnv
+    nixfmt
+    nixos-option
     nomachine-client
     ookla-speedtest
     p7zip
+    parallel-full
+    pfetch
+    pmutils
     powershell
     pulumi
+    python.pkgs.pip
+    python311Full
+    rar
     ripgrep
     ripgrep-all
+    rzip
     samba
     scala-cli
+    shotwell
     sl
     sshpass
     stow
     sublime4
+    system-config-printer # Allow for easy configuration of printers and print queues on the system
     tig
     tldr
     tree
+    unrar
     unzip
     vim
     vlc
+    vscode
     wget
     whatsapp-for-linux
     wl-clipboard
     wpsoffice
+    xdg-desktop-portal-gtk
     xfce.thunar
+    xz
     youtube-dl
+    zip
     zsh
+    zstd
   ];
 
-  # Fonts
+  # Custom fonts
   fonts.fonts = with pkgs; [
     noto-fonts
     noto-fonts-cjk
@@ -197,37 +212,143 @@
     source-han-serif-japanese
   ];
 
-  # List services and or custom Hardware additions:
+  # Provides a virtual file system for environment modules
+  services.envfs.enable = true;
 
-  #* Enable Bluetooth
+  # Define a set of programs and their respective configurations
+  programs = {
+    kdeconnect = { enable = true; };
+    dconf = { enable = true; };
+    mtr = { enable = true; };
+  };
+
+  # Provide a graphical Bluetooth manager for desktop environments
   hardware.bluetooth.enable = true;
   services.blueman.enable = true;
 
-  #* Enable Flatpak
+  # Enables support for Flatpak
   services.flatpak.enable = true;
 
-  #* Nvidia
+  # Enables the D-Bus service, which is a message bus system that allows communication between applications
+  services.dbus.enable = true;
+
+  # Nvidia drivers
   services.xserver.videoDrivers = [ "nvidia" ];
   hardware.opengl.enable = true;
   hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.stable;
   hardware.nvidia.modesetting.enable = true;
 
-  #* Add scanner drivers (Epson)
+  # Scanner drivers
   hardware.sane.enable = true;
   hardware.sane.extraBackends = [ pkgs.epkowa ];
-  #services.udev.packages = [ pkgs.utsushi ];
 
-  #* Printer Drivers
-  services.printing.drivers = [ pkgs.gutenprint ];
+  # Printers and drivers
   services.avahi.enable = true;
   services.avahi.nssmdns = true;
   services.avahi.openFirewall = true;
 
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  services.printing.enable = true;
+  services.printing.stateless = true;
+  services.printing.webInterface = false;
+
+  services.printing.drivers = with pkgs; [
+    epson-escpr
+    epson-escpr2
+    foomatic-db
+    foomatic-db-ppds
+    gutenprint
+    hplip
+    splix
+  ];
+
+  # System daemon for managing storage devices such as disks and USB drives
+  services.udisks2.enable = true;
+
+  # Enable the copying of system configuration files to the Nix store
+  # Automatic system upgrades, automatically reboot after an upgrade if necessary
   system.copySystemConfiguration = true;
   system.autoUpgrade.enable = true;
   system.autoUpgrade.allowReboot = true;
-  system.stateVersion = "23.05"; # Did you read the comment?
+  system.stateVersion = "23.05";
 
+  # Provide NetBIOS name resolution in local networks, allowing Windows devices to discover and connect to the NixOS system
+  services.samba-wsdd.enable = true;
+
+  # Adding a rule to the iptables firewall to allow NetBIOS name resolution traffic on UDP port 137
+  networking.firewall.extraCommands =
+    "iptables -t raw -A OUTPUT -p udp -m udp --dport 137 -j CT --helper netbios-ns";
+
+  # Samba Configuration
+  services.samba = {
+    enable = true;
+    package = pkgs.sambaFull;
+    openFirewall = true;
+    securityType = "user";
+    extraConfig = ''
+      workgroup = WORKGROUP
+      server string = smb-NixOs23
+      netbios name = smb-NixOs23
+      security = user
+      hosts allow = 192.168.0. 127.0.0.1 localhost
+      hosts deny = 0.0.0.0/0
+      guest account = nobody
+      map to guest = bad user
+      load printers = yes
+      printing = cups
+      printcap name = cups
+    '';
+
+    shares = {
+
+      # Home Directories Share
+      homes = {
+        comment = "Home Directories";
+        browseable = false;
+        "read only" = false;
+        "create mask" = "0700";
+        "directory mask" = "0700";
+        "valid users" = "%S";
+        writable = true;
+      };
+
+      # Public Share
+      NixOs23-KDE-Public = {
+        path = "/home/tolga/Public";
+        comment = "Public Share";
+        browseable = true;
+        "read only" = false;
+        "guest ok" = true;
+        writable = true;
+        "create mask" = "0777";
+        "directory mask" = "0777";
+        "force user" = "tolga";
+        "force group" = "samba";
+      };
+
+      # Private Share
+      NixOs23-KDE-Private = {
+        path = "/home/NixOs-KDE";
+        comment = "Private Share";
+        browseable = true;
+        "read only" = false;
+        "guest ok" = false;
+        "create mask" = "0644";
+        "directory mask" = "0755";
+        "force user" = "tolga";
+        "force group" = "samba";
+      };
+
+      # Printer Share
+      printers = {
+        comment = "All Printers";
+        path = "/var/spool/samba";
+        public = true;
+        browseable = true;
+        "guest ok" = true;
+        writable = false;
+        printable = true;
+        "create mask" = "0700";
+      };
+    };
+  };
 }
