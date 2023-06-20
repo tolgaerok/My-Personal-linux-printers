234e4c982cbe2c77b29d6f3d4ddd376c24ebce81
Tolga Erok <110285959+tolgaerok@users.noreply.github.com>
2023-06-20 10:42:34 +0800

    Update hardware-configuration.nix

--- LINUX RELATED/NIXOS 23-05 RELATED/System/BIOS/nixos/hardware-configuration.nix
+++ LINUX RELATED/NIXOS 23-05 RELATED/System/BIOS/nixos/hardware-configuration.nix
@@ -18,10 +18,10 @@
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
   boot.kernelParams = [ "mitigations=off" ];
-  # kernelPackages = pkgs.linuxPackages_latest;   # <====  Remove # to enable to update to the latest kernel automatically, use at own risk!
+  # kernelPackages = pkgs.linuxPackages_latest;   # Remove # to enable to update to the latest kernel automatically, use at own risk!
 
   fileSystems."/" = {
-    device = "/dev/disk/by-uuid/e465813a- XXXXXXXXXX";  # <==== Add your own uuid
+    device = "/dev/disk/by-uuid/e465813a-3a1a-4629-b2a4-d0ca24aaf10d"; # Add your own uuid
     fsType = "ext4";
     # for ssd
     options =
@@ -32,16 +32,17 @@
     device = "//192.168.0.20/LinuxData/HOME/PROFILES/NIXOS-23-05/TOLGA/";
     fsType = "cifs";
     options = let
-      automount_opts =
+      automountOpts =
         "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s,x-systemd.requires=network.target";
       uid =
-        "1000"; # <========= Use sudo id <YOUR USERNAME> to establish your uid
+        "1000"; # Replace with your actual user ID, use `id -u <YOUR USERNAME>` to get your user ID
       gid =
-        "100"; # <========= Use sudo id <YOUR USERNAME> to establish your gid
+        "100"; # Replace with your actual group ID, use `id -g <YOUR USERNAME>` to get your group ID
       vers = "3.1.1";
-      cache_opts = "cache=loose";
+      cacheOpts = "cache=loose";
+      credentialsPath = "/etc/nixos/smb-secrets";
     in [
-      "${automount_opts},credentials=/etc/nixos/smb-secrets,uid=${uid},gid=${gid},vers=${vers},${cache_opts}"
+      "${automountOpts},credentials=${credentialsPath},uid=${uid},gid=${gid},vers=${vers},${cacheOpts}"
     ];
   };
 
